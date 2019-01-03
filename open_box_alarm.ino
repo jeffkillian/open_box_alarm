cf5357279c8243d117df892de89914ce2d055804
jeffkillian <jeffkillian@gmail.com>
2019-01-02 21:49:00 -0800

    ADDING POLICE SIREN AND LIGHTS

--- open_box_alarm.ino
+++ open_box_alarm.ino
@@ -1,21 +1,33 @@
 const int photoresistor = A0;
-const int piezo = 9;
+const int piezoPin = 9;
+const int greenPin = 6;
+const int redPin = 11;
 int photovalue;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(piezo, OUTPUT);
-  pinMode(photoresistor, INPUT); }
+  pinMode(piezoPin, OUTPUT);
+  pinMode(photoresistor, INPUT); 
+    pinMode(redPin, OUTPUT);
+     pinMode(greenPin, OUTPUT);
+}
 
+// Plays a police siren loop and flashes lights in unison
 void loop() {
   
   photovalue = analogRead(photoresistor);
   Serial.println(photovalue);
-  
-  if (photovalue > 300) {
-    digitalWrite(piezo, HIGH); 
+  if (photovalue > 100) {
+    tone(piezoPin,  739.99, 500); //F#5
+    digitalWrite(redPin, HIGH); 
+    digitalWrite(greenPin, LOW); 
+    delay(500);
+    tone(piezoPin,  523.25, 500); //C5
+    digitalWrite(redPin, LOW); 
+   digitalWrite(greenPin, HIGH ); 
+    delay(500);
   }
   else {
-    digitalWrite(piezo, LOW); 
+    digitalWrite(piezoPin, LOW); 
   }
 }
