7fc30237dffdd6a9f8d82a2f009d12eb1ea60702
jeffkillian <jeffkillian@gmail.com>
2019-01-02 21:19:00 -0800

    Just fixing spacing

--- open_box_alarm.ino
+++ open_box_alarm.ino
@@ -3,19 +3,19 @@ const int piezo = 9;
 int photovalue;
 
 void setup() {
-   Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(piezo, OUTPUT);
   pinMode(photoresistor, INPUT); }
 
 void loop() {
   
-     photovalue = analogRead(photoresistor);
-     Serial.println(photovalue);
+  photovalue = analogRead(photoresistor);
+  Serial.println(photovalue);
   
-      if (photovalue > 300) {
-        
-        digitalWrite(piezo, HIGH); 
-        }
-
-     else { digitalWrite(piezo, LOW); }
-   }
+  if (photovalue > 300) {
+    digitalWrite(piezo, HIGH); 
+  }
+  else {
+    digitalWrite(piezo, LOW); 
+  }
+}
